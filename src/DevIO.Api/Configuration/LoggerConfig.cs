225f168c8921377c527e6d4c876678387969e130
Guibolt <guilhermedosreiss@gmail.com>
2021-09-07 15:44:56 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.Api/Configuration/LoggerConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api.Configuration
+{
+    public static class LoggerConfig
+    {
+        const string chaveApiLog = "64382751abb34cfd8ec13b644fd7bd22";
+        const string chaveLog = "e1efec4f-c395-49fc-ad8e-808ac154f0f5";
+        public static IServiceCollection AddLoggingConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddElmahIo(o =>
+            {
+                o.ApiKey = chaveApiLog;
+                o.LogId = new Guid(chaveLog);
+            });
+
+            services.AddHealthChecks()
+           .AddCheck("Produtos", new SqlServerHealthCheck(connection))
+           .AddSqlServer(connection);
+            services.AddHealthChecksUI();
+
+            //services.AddHealthChecks()
+            //    .AddElmahIoPublisher(options =>
+            //    {
+            //        options.ApiKey = "388dd3a277cb44c4aa128b5c899a3106";
+            //        options.LogId = new Guid("c468b2b8-b35d-4f1a-849d-f47b60eef096");
+            //        options.HeartbeatId = "API Fornecedores";
+
+            //    })
+            //    .AddCheck("Produtos", new SqlServerHealthCheck(configuration.GetConnectionString("DefaultConnection")))
+            //    .AddSqlServer(configuration.GetConnectionString("DefaultConnection"), name: "BancoSQL");
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseLoggingConfiguration(this IApplicationBuilder app)
+        {
+            app.UseElmahIo();
+
+            return app;
+        }
+    }
+}
---
+++ src/DevIO.Api/Startup.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using DevIO.Api.Configuration;
+using DevIO.Api.Extensions;
+using DevIO.Data.Context;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace DevIO.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        
+            services.AddIdentityConfig(Configuration);
+            services.AddAutoMapper(typeof(Startup));
+
+            services.WebApiConfig();
+            services.AddLoggingConfig(Configuration);
+            services.AddSwaggerConfig();
+            services.ResolveDependencies();
+        }
+
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+           
+            else
+                app.UseHsts();
+            
+            app.UseAuthentication();
+            app.UseMvcConfiguration();
+            app.UseLoggingConfiguration();
+            app.UseSwaggerConfig(provider);
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseHealthChecks("/api/hc", new HealthCheckOptions
+            {
+                Predicate = p => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            }
+            ); 
+
+            app.UseHealthChecksUI(opt => { opt.UIPath = "/api/hc-ui"; });
+
+        }
+    }
+}
